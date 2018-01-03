694b7963cd4a838b3fdb2b569e4406ab781a7f34
thinkbed <dreamflylin@gmail.com>
2018-01-03 23:31:29 +0800

    webpack to support react

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,25 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
-    entry: './index.js',
+    entry: './index.jsx',
     output: {
         filename: 'bundle.js'
     },
     module: {
         rules: [
-            {test: /\.css$/, use: [ {loader: 'style-loader'}, {loader: 'css-loader'} ]}
+            {test: /\.css$/, use: [ {loader: 'style-loader'}, {loader: 'css-loader'} ]},
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['env', 'react']
+                }
+            },
+            {
+                test: /\.scss$/,
+                loaders: ['style-loader', 'css-loader', 'sass-loader']
+            }
         ]
     },
     devServer: {
@@ -22,5 +34,8 @@ module.exports = {
             title: 'Test App',
         }),
         new webpack.HotModuleReplacementPlugin(),
-    ]
+    ],
+    resolve: {
+        extensions: ['.js', '.jsx']
+    }
 }
